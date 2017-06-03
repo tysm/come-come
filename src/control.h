d346b381b0bf15dc8f3b1dde65de95a7abc34a6b
Thalles Yan Santos Medrado <thallesyann@hotmail.com>
2017-06-02 21:34:18 -0300

    Control

--- src/control.h
+++ src/control.h
@@ -1,11 +1,1 @@
-#include <stdint.h>
-
-#if defined(_WIN32)
-#   define PLATFORM_WIN32
-#elif defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
-#   define PLATFORM_UNIX
-#else
-#   error Unsupported platform
-#endif
-
 extern void player_control(float *x, float *y, char screen[24][80]);
